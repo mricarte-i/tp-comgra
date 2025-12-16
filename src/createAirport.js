1cbda0887fb6c5f229b8203bbda911b787850055
Matias Ricarte <mricarte@itba.edu.ar>
2025-12-15 21:34:53 -0300

    hangar texturing

--- src/createAirport.js
+++ src/createAirport.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
 
 // helper to load a texture as a Promise
 function loadTextureAsync(path) {
@@ -192,17 +193,6 @@ export async function createAirport(
   // hangars
   /////////////////
   const hangars = new THREE.Group();
-  function createHangar() {
-    const hangarBase = new THREE.Mesh(
-      new THREE.CylinderGeometry(3, 3, 6, 16),
-      new THREE.MeshStandardMaterial({
-        color: 0x999999,
-        side: THREE.DoubleSide,
-      })
-    );
-    hangarBase.rotation.set(0, 0, Math.PI / 2);
-    return hangarBase;
-  }
   for (let i = 0; i < 3; i++) {
     const hangar = createHangar();
     hangar.position.set(-10, 5, 15 + i * 7);
@@ -227,5 +217,182 @@ export async function createAirport(
     camOrbitTower,
     camOrbitTowerControls,
     antennaLight,
+  };
+}
+
+function createHangar() {
+  const radius = 3;
+  const height = 6;
+  const radialSegments = 16;
+  let parts = makeSplitCylinder(radius, height, radialSegments);
+  //merge geometries
+  let geometry = BufferGeometryUtils.mergeGeometries(
+    [parts.walls, parts.caps.top, parts.caps.bottom],
+    false
+  );
+  // get counts for later
+  const countWalls =
+    (parts.walls.attributes.position.count - 1) * 3;
+  const countTop = parts.caps.top.attributes.position.count * 3;
+  const countBottom =
+    parts.caps.bottom.attributes.position.count * 3;
+
+  // override uvs to map the entire texture on each part
+  let uvs = [];
+  // walls
+  for (let i = 0; i < parts.walls.attributes.uv.count; i++) {
+    // get current uv
+    let u = parts.walls.attributes.uv.getX(i);
+    let v = parts.walls.attributes.uv.getY(i);
+    // get vertex position
+    let x = parts.walls.attributes.position.getX(i);
+    let y = parts.walls.attributes.position.getY(i);
+    let z = parts.walls.attributes.position.getZ(i);
+
+    // definir aqui el mapeo UV para las paredes
+    u = y / height + 0.5;
+    v = -z / height + 0.5;
+
+    uvs.push(u);
+    uvs.push(v);
+  }
+  // top
+  for (let i = 0; i < parts.caps.top.attributes.uv.count; i++) {
+    // get current uv
+    let u = parts.caps.top.attributes.uv.getX(i);
+    let v = parts.caps.top.attributes.uv.getY(i);
+    // get vertex position
+    let x = parts.caps.top.attributes.position.getX(i);
+    let y = parts.caps.top.attributes.position.getY(i);
+    let z = parts.caps.top.attributes.position.getZ(i);
+
+    // definir aqui el mapeo UV para la tapa superior
+    u = x / (2 * radius) + 0.5;
+    v = -z / (2 * radius) + 0.5;
+
+    uvs.push(u);
+    uvs.push(v);
+  }
+  // bottom
+  for (
+    let i = 0;
+    i < parts.caps.bottom.attributes.uv.count;
+    i++
+  ) {
+    // get current uv
+    let u = parts.caps.bottom.attributes.uv.getX(i);
+    let v = parts.caps.bottom.attributes.uv.getY(i);
+    // get vertex position
+    let x = parts.caps.bottom.attributes.position.getX(i);
+    let y = parts.caps.bottom.attributes.position.getY(i);
+    let z = parts.caps.bottom.attributes.position.getZ(i);
+
+    // definir aqui el mapeo UV para la tapa inferior
+    u = -x / (2 * radius) + 0.5;
+    v = -z / (2 * radius) + 0.5;
+
+    uvs.push(u);
+    uvs.push(v);
+  }
+  geometry.setAttribute(
+    'uv',
+    new THREE.Float32BufferAttribute(uvs, 2)
+  );
+
+  // load /maps/cap_map.jpg and wall_map.jpg as textures
+  let textureLoader = new THREE.TextureLoader();
+  let wallTexture = textureLoader.load('/airport/wall_map.jpg');
+  let capTexture = textureLoader.load('/airport/cap_map.jpg');
+  let uvTexture = textureLoader.load('/uv.jpg');
+  // set repeat for all textures
+  uvTexture.wrapS = THREE.RepeatWrapping;
+  uvTexture.wrapT = THREE.RepeatWrapping;
+
+  capTexture.wrapS = capTexture.wrapT = THREE.RepeatWrapping;
+  wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
+
+  let wallNormal = textureLoader.load(
+    '/airport/wall_map-normal.jpg'
+  );
+  wallNormal.wrapS = THREE.RepeatWrapping;
+  wallNormal.wrapT = THREE.RepeatWrapping;
+
+  let capNormal = textureLoader.load(
+    '/airport/cap_map-normal.jpg'
+  );
+  capNormal.wrapS = THREE.RepeatWrapping;
+  capNormal.wrapT = THREE.RepeatWrapping;
+
+  // set different textures for walls and caps using groups
+  const materials = [];
+  materials.push(
+    new THREE.MeshStandardMaterial({
+      map: wallTexture,
+      normalMap: wallNormal,
+    })
+  ); // walls
+  materials.push(
+    new THREE.MeshStandardMaterial({
+      map: capTexture,
+      normalMap: capNormal,
+    })
+  ); // top
+
+  geometry.addGroup(0, countWalls, 0); // walls
+  geometry.addGroup(countWalls - 3, countTop + countBottom, 1); // caps
+
+  let cylinder = new THREE.Mesh(geometry, materials);
+  cylinder.rotation.z = Math.PI / 2;
+  cylinder.rotation.x = Math.PI / 2;
+  return cylinder;
+}
+
+// source: https://github.com/fedemarino31/cg7258/blob/main/demoMapeoUV/src/main.js
+// Crea un cilindro dividido en paredes y tapas.
+// radio: número (requerido)
+// altura: número (requerido)
+// radialSegments: número (opcional, default 64)
+function makeSplitCylinder(radius, height, radialSegments = 64) {
+  const walls = new THREE.CylinderGeometry(
+    radius, // radiusTop
+    radius, // radiusBottom
+    height, // height
+    radialSegments, // radialSegments
+    1, // heightSegments
+    true // openEnded
+  );
+
+  // --- TAPAS ---
+  const top = new THREE.CircleGeometry(radius, radialSegments);
+  top.rotateX(-Math.PI / 2);
+  top.translate(0, height / 2, 0);
+
+  const bottom = new THREE.CircleGeometry(
+    radius,
+    radialSegments
+  );
+  bottom.rotateX(Math.PI / 2);
+  bottom.translate(0, -height / 2, 0);
+
+  function scaleV(geometry, factor = 10) {
+    const uv = geometry.attributes.uv;
+    if (uv) {
+      for (let i = 0; i < uv.count; i++) {
+        uv.setY(i, uv.getY(i) * factor);
+      }
+      uv.needsUpdate = true;
+    }
+  }
+
+  scaleV(walls);
+  scaleV(top);
+  scaleV(bottom);
+
+  return {
+    walls,
+    caps: {
+      top,
+      bottom,
+    },
   };
 }
